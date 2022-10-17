2f56a57237c023f5dd53b9d63770fbcfbee39e5f
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-10-17 17:18:48 -0300

    Implementando um repositório SDJ customizado 5.11

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
