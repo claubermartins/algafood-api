24b272ca51cbd0bd044b8bd5235033b0e722f163
“claubermartins” <clauber789@gmail.com>
2023-05-30 16:33:53 -0300

    8.14. Desafio implementando exception handler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
@@ -40,6 +41,16 @@ public class ApiExceptionHandler {
 				.mensagem("O tipo de mídia não é aceito").build();
 		
 		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("A unidade não pode ser removida, pois está em uso").build();
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT)
 				.body(problema);
 	}
 }
