59ed17c4f77dceed6785f8f9a6bc47ddadef58ec
“claubermartins” <clauber789@gmail.com>
2023-07-17 15:24:14 -0300

    9.5. Conhecendo e adicionando mais constraints de validação no modelo

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -16,7 +16,8 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -36,10 +37,11 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
+	@DecimalMin("1")
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete; 
 	
