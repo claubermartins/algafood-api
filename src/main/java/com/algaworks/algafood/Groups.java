6ad63c08b5e7161ca6c3c6fe54afda9fc7008eae
“claubermartins” <clauber789@gmail.com>
2023-08-22 15:05:15 -0300

    9.9. Desafio adicionando constraints de validação no modelo

--- src/main/java/com/algaworks/algafood/Groups.java
+++ src/main/java/com/algaworks/algafood/Groups.java
@@ -2,7 +2,9 @@ package com.algaworks.algafood;
 
 public interface Groups {
 	
-	public interface CadastroRestaurante{
-		
-	}
+	public interface CadastroRestaurante{}
+	
+	public interface CozinhaId {}
+	
+	public interface EstadoId {}
 }
