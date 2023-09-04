74c322e8b25bfa8b1b8d455a51e36e777e7ff865
“claubermartins” <clauber789@gmail.com>
2023-09-04 15:02:42 -0300

    9.11. Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,6 +51,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -58,10 +64,13 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	    BindingResult bindingResult = ex.getBindingResult();
 	   
 	    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
-	    		.map(fieldError -> Problem.Field.builder()
+	    		.map(fieldError -> {
+	    			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+	    			return Problem.Field.builder()
 	    				.name(fieldError.getField())
-	    				.userMessage(fieldError.getDefaultMessage())
-	    				.build())
+	    				.userMessage(message)
+	    				.build();
+	    		})
 	    		.collect(Collectors.toList());
 	    
 	    Problem problem = createProblemBuilder(status, problemType, detail)
