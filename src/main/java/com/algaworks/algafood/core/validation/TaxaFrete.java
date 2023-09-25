34364b7a8fb8eed2ba17a63e2eb59d2e59d076ae
“claubermartins” <clauber789@gmail.com>
2023-09-25 16:10:52 -0300

    9.15. Criando constraints de validação customizadas usando composição

---
+++ src/main/java/com/algaworks/algafood/core/validation/TaxaFrete.java
@@ -0,0 +1,53 @@
+package com.algaworks.algafood.core.validation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.OverridesAttribute;
+import javax.validation.Payload;
+import javax.validation.constraints.PositiveOrZero;
+
+/**
+ * The annotated element must be a positive number or 0.
+ * <p>
+ * Supported types are:
+ * <ul>
+ *     <li>{@code BigDecimal}</li>
+ *     <li>{@code BigInteger}</li>
+ *     <li>{@code byte}, {@code short}, {@code int}, {@code long}, {@code float},
+ *     {@code double} and their respective wrappers</li>
+ * </ul>
+ * <p>
+ * {@code null} elements are considered valid.
+ *
+ * @author Gunnar Morling
+ * @since 2.0
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = { })
+@PositiveOrZero
+public @interface TaxaFrete {
+	
+	@OverridesAttribute(constraint = PositiveOrZero.class, name = "message")
+	String message() default "{TaxaFrete.invalida}";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+}
+
+
+
+
+
+
